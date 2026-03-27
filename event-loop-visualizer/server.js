3814676479d77d03cb636295161391bb57f3dc94
Pheetomilechi Ghajiga <tomilechighajiga@gmail.com>
2026-03-27 10:08:47 +0000

    Created Endpoint 1 which carries out Event loop blocking

--- event-loop-visualizer/server.js
+++ event-loop-visualizer/server.js
@@ -4,7 +4,7 @@ const { default: chalk } = require('chalk');
 const app = express();
 const PORT = 3000;
 
-//Helper for visual logging
+// Helper for visual logging
 function logWithColor(message, type, requireId) {
     const timestamp = new Date().toISOString().split('T') [1];
     const colors = {
@@ -15,4 +15,32 @@ function logWithColor(message, type, requireId) {
         'async': chalk.green,
         'io': chalk.cyan
     };
+    const colorFn = colors[type] || chalk.white;
+    console.log(colorFn(`[${timestamp}] [${type.toUpperCase()}]
+    [${requestId}] $(message)`));
+
+    // Middleware to track request IDs
+    let requestCounter = 0;
+    app.use((req, res, next) => {
+        req.id = ++requestCounter;
+        logWithColor(`Request ${req.method} ${req.url} received`,
+        'event-loop', req.id);
+        next();
+    });
+
+    // Endpoint 1: Synchronous - Event Loop Blocking
+    app.get('/blocking/time', (req, res) => {
+        const time = parsInt(req.params.time) || 3000;
+        const end = Date.now() + time;
+
+        logWithColor(`Starting BLOCKING operation
+        for ${time}ms`, 'blocking', req.id);
+
+        // Block the event loop
+        while (Date.now() < end) {
+            // CPU-intensive loop
+        }
+        logWithColor(`Blocking operation COMPLETED`, 'blocking', req.id);
+        res.send(`Blocked the event loop for ${times}ms`);
+    })
 }
